0b7e96a725a92889cb1a67bdb9d6c5bcf794c6a1
marcusp619 <marcusp619@gmail.com>
2017-03-25 23:20:34 -0600

    First component
    es6 class setup for grabbing the methods from Component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React, { Component } from 'react';
 
+/* es6 class setup for grabbing the methods from Component
+** A component in react is a independant and reuseable piece of ui
+** JSX is javascript that allows us to right components
+** Importing and exporting is what allows us to share code to other components
+*/
+class App extends Component {
+  // render method returns this to the view
+  render() {
+    return (
+      <div>Countdown Champ, App Component</div>
+    )
+  }
+}
+
+export default App;
