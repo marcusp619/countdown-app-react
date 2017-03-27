65392d49088eb4d0737a1a56e96e44478c162abe
marcusp619 <marcusp619@gmail.com>
2017-03-26 23:49:41 -0600

    Created clock component
    Created clock component 
    Added state for the days,hours,mins,secs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clock from './Clock.jsx'
 
 /* es6 class setup for grabbing the methods from Component
 ** A component in react is a independant and reuseable piece of ui
@@ -24,13 +25,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to {this.state.deadline}</div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
+        <div className="App-title">Countdown to {this.state.deadline}
         </div>
+        <Clock />
         <div>
           <input
             placeholder='new date'
