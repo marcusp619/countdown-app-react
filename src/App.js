48d0ae00b65a462dfa542c2762d2d9d128b204c5
marcusp619 <marcusp619@gmail.com>
2017-04-02 01:54:16 -0600

    Added react-bootstrap
    Just a little styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Clock from './Clock.jsx'
+import Clock from './Clock.jsx';
+import { Form, FormControl, Button} from 'react-bootstrap';
 
 /* es6 class setup for grabbing the methods from Component
 ** A component in react is a independant and reuseable piece of ui
@@ -31,15 +32,16 @@ class App extends Component {
           // inherits from its parent Component App.js
           deadline={this.state.deadline}
         />
-        <div>
-          <input
-            placeholder='new date'
-            onChange={event => this.setState({newDeadline: event.target.value})}
-          />
-          <button onClick={() => this.changeDeadline()}>
-            Submit
-          </button>
-        </div>
+          <Form inline>
+            <FormControl
+              className='Deadline-input'
+              placeholder='new date'
+              onChange={event => this.setState({newDeadline: event.target.value})}
+            />
+          <Button onClick={() => this.changeDeadline()}>
+              Submit
+            </Button>
+          </Form>
       </div>
     )
   }
